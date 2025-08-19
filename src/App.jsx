f019da217a11a64ee92a5577e1b0aebf07be9f1c
Tommy Clark <tommyclark362@gmail.com>
2025-08-19 22:37:55 +0100

    Initial commit: Portfolio project with React, Vite, and Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { Home } from "lucide-react"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { NotFound } from "./pages/NotFound"
+
+function App () {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />}/>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      
+      
+      </BrowserRouter>
+    
+    
+    
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+
+export const Home = () => {
+    return <div className="min-h-screen>">
+        {/* Theme Toggle */}
+
+        {/* Backround Effects */}
+
+        {/* NavBar */}
+
+        {/* Main Content */}
+
+ 
+        {/* Footer */}
+
+
+    </div>
+}
