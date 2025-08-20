190ee8cc6116d716671d6e037adf796647fbe00b
tommyc10 <tommyclark362@gmail.com>
2025-08-20 14:11:53 +0100

    updates to indexcss and homejsx for styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 
 export const Home = () => {
-    return <div className="min-h-screen>">
+    return <div className="min-h-screen bg-backround text-foreground overflow-x-hidden>">
         {/* Theme Toggle */}
 
         {/* Backround Effects */}
