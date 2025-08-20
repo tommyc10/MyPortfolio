d8287fa14ef149b31bff205f54cc3189f2a8a22d
Tommy Clark <tommyclark362@gmail.com>
2025-08-20 16:02:26 +0100

    added rezize feature for devices

--- src/Components/StarBackground.jsx
+++ src/Components/StarBackground.jsx
@@ -12,6 +12,14 @@ export const StarBackground = () => {
     useEffect(() => {
         generateStars();
         generateMeteors();
+
+        const handleResize = () => {
+            generateStars();
+        };
+
+        window.addEventListener("resize", handleResize)
+
+        return () => window.removeEventListener("resize", handleResize);
     }, []);
 
     const generateStars  = () => {
